b36c87e04ffd681207c4141552d7da5f003e12e9
Kristen <kristengamelin@gmail.com>
2021-09-28 18:00:58 -0400

    transactions Controller create worked on

--- GeneralStore.MVC/Controllers/TransactionController.cs
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -22,6 +22,21 @@ namespace GeneralStore.MVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            if (transaction.Quantity <= 0)
+            { // have to buy at least one
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = await
+        }
+
     }
 
 }

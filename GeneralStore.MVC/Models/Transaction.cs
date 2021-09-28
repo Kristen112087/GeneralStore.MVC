9913c19470db1e802bb8252f4e88aedbe8e9d923
Kristen <kristengamelin@gmail.com>
2021-09-28 15:02:39 -0400

    transaction Controller, View and Model added

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+        [Required]
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+    }
+}

45debdfebb5ef8d2eb7bb7caeb24d67318a2a86b
Kristen <kristengamelin@gmail.com>
2021-09-28 13:28:35 -0400

    customer view and controller added

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // Adding the application DB Context (the link to the database - without this, controllers can not talk to or use the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
